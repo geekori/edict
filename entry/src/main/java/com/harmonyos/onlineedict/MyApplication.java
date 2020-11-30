06e03aff4be5e4782226c816b3bc6dc09487faff
lining <helloworld@126.com>
2020-11-30 17:40:19 +0800

    update

--- entry/src/main/java/com/harmonyos/onlineedict/MyApplication.java
+++ entry/src/main/java/com/harmonyos/onlineedict/MyApplication.java
@@ -1,9 +1,9 @@
-/*
-作者：李宁（蒙娜丽宁)
-Copyright © 2020 Lining. All rights reserved.
-更多关于鸿蒙的精彩内容，请关注微信公众号【极客起源】
-更多关于鸿蒙的精彩视频请关注我的B站：https://space.bilibili.com/477001733
- */
+// 作者：李宁（蒙娜丽宁)
+// Copyright © 2020 Lining. All rights reserved.
+// 更多关于鸿蒙的精彩内容，请关注微信公众号【极客起源】
+// 项目完整教学视频：https://space.bilibili.com/477001733
+// 更多关于鸿蒙的精彩视频请关注我的B站：https://space.bilibili.com/477001733
+
 package com.harmonyos.onlineedict;
 
 import ohos.aafwk.ability.AbilityPackage;
